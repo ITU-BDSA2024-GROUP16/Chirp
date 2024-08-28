ae76743d9d0af348f31f1cbd2c1cc4b598c802cd
Jacques <japu@itu.dk>
2024-08-28 15:12:30 +0200

    Test commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿foreach (var arg in args)
-    Console.WriteLine(arg);
+﻿
+
+try
+{
+    using StreamReader reader = new("C:\\Users\\jacqu\\Chirp.CLI\\chirp_cli_db.csv");
+    String text = reader.ReadToEnd();
+    
+    Console.WriteLine(text);
+}
+catch (IOException e)
+{
+    Console.WriteLine("This file could not be read:");
+    //Console.WriteLine(e);
+}
