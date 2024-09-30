9b9d18672f68567c6da968691c17ad53fce0e0f8
Noale <noan@itu.dk>
2024-09-30 16:00:00 +0200

    Database related code is now incapsulated in class DBFacade.cs
    Co-Authored-By: Johan Ingeholm <151632146+JIngeholm@users.noreply.github.com>

--- CheepService.cs
+++ CheepService.cs
@@ -1,6 +1,6 @@
 
 using System.Data;
-using Microsoft.Data.Sqlite;
+using Chirp.Razor.Initial;
 public record CheepViewModel(string Author, string Message, string Timestamp);
 
 public interface ICheepService
@@ -17,43 +17,8 @@ public class CheepService : ICheepService
         SQLitePCL.Batteries.Init();
     }
     // These would normally be loaded from a database for example
-    private static readonly List<CheepViewModel> _cheeps = LoadCheeps();
-
-    private static List<CheepViewModel> LoadCheeps()
-    {
-        var cheeps = new List<CheepViewModel>();
-        var sqlDBFilePath = "/tmp/chirp.db"; 
-
-        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
-        {
-            connection.Open();
-            
-           // var sqlQuery = "SELECT author_id, message_id, pub_date FROM message ORDER BY pub_date DESC"; // Adjust to match your table structure
-            var sqlQuery = "SELECT u.username, m.author_id, m.text, m.pub_date FROM user u JOIN message m ON u.user_id = m.author_id ORDER BY m.pub_date DESC";
-            using (var command = new SqliteCommand(sqlQuery, connection))
-            {
-                using var reader = command.ExecuteReader();
-                while (reader.Read())
-                {
-                    long pubDateUnix = reader.GetInt64(reader.GetOrdinal("pub_date"));
-                    DateTime pubDate = DateTimeOffset.FromUnixTimeSeconds(pubDateUnix).DateTime;
-
-                    // Create CheepViewModel and add to the list
-                    var cheep = new CheepViewModel(
-                        reader.GetString(reader.GetOrdinal("username")),
-                        reader.GetString(reader.GetOrdinal("text")),
-                        pubDate.ToString("MM/dd/yy H:mm:ss")
-                        //reader.GetDateTime(reader.GetOrdinal("pub_date")).ToString("MM/dd/yy H:mm:ss")
-                    );
-
-                    cheeps.Add(cheep); // Add the created CheepViewModel to the list
-                }
-            }
-        }
-
-        return cheeps; // Return the list of CheepViewModel objects
-    }
-
+    private static readonly List<CheepViewModel> _cheeps = DBFacade.LoadCheeps();
+    
 
     public List<CheepViewModel> GetCheeps()
     {
@@ -65,7 +30,7 @@ public class CheepService : ICheepService
         // filter by the provided author name
         return _cheeps.Where(x => x.Author == author).ToList();
     }
-
+/*
     private static string UnixTimeStampToDateTimeString(double unixTimeStamp)
     {
         // Unix timestamp is seconds past epoch
@@ -73,5 +38,6 @@ public class CheepService : ICheepService
         dateTime = dateTime.AddSeconds(unixTimeStamp);
         return dateTime.ToString("MM/dd/yy H:mm:ss");
     }
+    */
 
 }
---
+++ DBFacade.cs
@@ -0,0 +1,44 @@
+using Microsoft.Data.Sqlite;
+
+namespace Chirp.Razor.Initial;
+
+public class DBFacade
+{
+    public static List<CheepViewModel> LoadCheeps()
+    {
+        var cheeps = new List<CheepViewModel>();
+        var sqlDBFilePath = "/tmp/chirp.db"; 
+
+        using (var connection = new SqliteConnection($"Data Source={sqlDBFilePath}"))
+        {
+            connection.Open();
+            
+            
+            // var sqlQuery = "SELECT author_id, message_id, pub_date FROM message ORDER BY pub_date DESC"; // Adjust to match your table structure
+            var sqlQuery = "SELECT u.username, m.author_id, m.text, m.pub_date FROM user u JOIN message m ON u.user_id = m.author_id ORDER BY m.pub_date DESC";
+            using (var command = new SqliteCommand(sqlQuery, connection))
+            {
+                using var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    long pubDateUnix = reader.GetInt64(reader.GetOrdinal("pub_date"));
+                    DateTime pubDate = DateTimeOffset.FromUnixTimeSeconds(pubDateUnix).DateTime;
+
+                    // Create CheepViewModel and add to the list
+                    var cheep = new CheepViewModel(
+                        reader.GetString(reader.GetOrdinal("username")),
+                        reader.GetString(reader.GetOrdinal("text")),
+                        pubDate.ToString("MM/dd/yy H:mm:ss")
+                        //reader.GetDateTime(reader.GetOrdinal("pub_date")).ToString("MM/dd/yy H:mm:ss")
+                    );
+
+                    cheeps.Add(cheep); // Add the created CheepViewModel to the list
+                }
+            }
+        }
+
+        return cheeps; // Return the list of CheepViewModel objects
+    }
+    
+    
+}
