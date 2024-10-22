598b4bc9c78c21f672e5ec790e0de021db192c6a
Freya Nørlund Wentzel <frnw@itu.dk>
2024-10-22 14:19:18 +0200

    Constraining the length of cheeps

--- src/Chirp.Core/Cheep.cs
+++ src/Chirp.Core/Cheep.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Core;
 
 public class Cheep
 {
     public int CheepId { get; set; }
-    public string Text { get; set; }
+    
+    [Required]
+    [StringLength(160)]
+    public required string Text { get; set; }
     public DateTime TimeStamp { get; set; }
     
     //Put AuthorId as foreign key form Author. DbInitializer could not handle the value unless this was put
