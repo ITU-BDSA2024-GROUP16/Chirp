a2d6800b59281f0dc159c24b32b126723933e412
Jacques <japu@itu.dk>
2024-10-29 14:34:09 +0100

    Scaffolded Register
    Register.cshtml is visible Identity/Pages/Account

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Chirp.Web.Areas.Identity.Data;
+
 using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
