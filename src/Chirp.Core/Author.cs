533589a6e3d460e58d27daba467a3e013aa2f201
JIngeholm <jing@itu.dk>
2024-10-30 20:03:15 +0100

    Removes duplicate email variable, so that the account info page displays the email in the database.

--- src/Chirp.Core/Author.cs
+++ src/Chirp.Core/Author.cs
@@ -7,7 +7,6 @@ namespace Chirp.Core
     {
     public int AuthorId { get; set; }
     public string? Name { get; set; }
-    public string? Email { get; set; }
     public ICollection<Cheep>? Cheeps { get; set; }
     }
 }
